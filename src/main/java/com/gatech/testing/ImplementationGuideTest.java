064fd8f0af30bd975f92b8419c308289687a5b72
Nikesh Ghimire <nikesh.ghimire@wnco.com>
2021-11-03 10:35:47 -0500

    Added missing files and methods

---
+++ src/main/java/com/gatech/testing/ImplementationGuideTest.java
@@ -0,0 +1,29 @@
+package com.gatech.testing;
+
+import com.gatech.services.parser.ImplementationGuide;
+import org.apache.catalina.mapper.Mapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class ImplementationGuideTest {
+
+
+    public ImplementationGuideTest() {
+    }
+
+    public void testFunctionality() throws IOException, ParseException {
+        ImplementationGuide implementationGuide = new ImplementationGuide();
+
+        final JSONObject data = implementationGuide.readImplementationGuide("src/main/java/com/gatech/data/implementationGuide/us-core.json");
+
+        JSONObject snapshot = (JSONObject) data.get("snapshot");
+        JSONArray element = (JSONArray) snapshot.get("element");
+        System.out.println(implementationGuide.findMustSupport(element));
+        System.out.println(implementationGuide.findMustHave(element));
+        System.out.println(implementationGuide.findValuesInCode("time"));
+        System.out.println(implementationGuide.findResourceType(data));
+    }
+}
